db1a45ffc0c9236d0086ae476cfd5c6ecc6c6c0f
urvivyas02 <40445170+urvivyas02@users.noreply.github.com>
2018-06-21 10:32:02 +0530

    Add files via upload

---
+++ WeatherApplication/db.sql
@@ -0,0 +1,98 @@
+-- MySQL dump 10.13  Distrib 5.6.40, for Win32 (AMD64)
+--
+-- Host: localhost    Database: userDB
+-- ------------------------------------------------------
+-- Server version	5.6.40-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dummy`
+--
+
+DROP TABLE IF EXISTS `dummy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dummy` (
+  `id` varchar(10) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dummy`
+--
+
+LOCK TABLES `dummy` WRITE;
+/*!40000 ALTER TABLE `dummy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dummy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `uname` varchar(20) NOT NULL,
+  `pass` varchar(20) NOT NULL,
+  `fname` varchar(20) DEFAULT NULL,
+  `lname` varchar(20) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('Hitesh','wea','Hitesh','Wadhwani','wadhwani.hitesh2011@gmail.com'),('uv','123','urvi','vyas','urvi.vyas@abc.com'),('rb','123','Rose','Bolton','rb@gmail.com'),('ak','123','Akshay','wadhwani','ak@yaho.com'),('ansh','deep','Anshul','vyas','anvy@gmail.com'),('nv','1','Nutan','vyas','nv@gmail.com'),('dp','1','Deepika','vyas','dp@abc.com');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_history`
+--
+
+DROP TABLE IF EXISTS `user_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_history` (
+  `uname` varchar(20) DEFAULT NULL,
+  `history` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_history`
+--
+
+LOCK TABLES `user_history` WRITE;
+/*!40000 ALTER TABLE `user_history` DISABLE KEYS */;
+INSERT INTO `user_history` VALUES ('Hitesh','city#1529533407230;Toronto,ca#1529533856601;Toronto,ca#1529533889734;Indore,in#1529533967529;city#1529534987433'),('uv','city#1529535053696;city,au#1529535063625;Toronto,ca#1529535576281'),('rb','Toronto,ca#1529535693203;Toronto,ca#1529535734361'),('ak','Toronto,ca#1529536264505'),('ansh','Toronto,ca#1529536479591'),('nv','Toronto,ca#1529537389857');
+/*!40000 ALTER TABLE `user_history` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-21  5:52:34
